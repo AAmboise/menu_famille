542850391eac9c11f7be3ce2b9a11a9932683760
AAmboise <93740922+AAmboise@users.noreply.github.com>
2022-01-22 00:29:02 +0100

    build(twig): add twig + base

---
+++ src/controller/home.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../../template');
+
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+$texte = 'Coucou';
+
+echo $twig->render('home/index.html.twig', [
+    'texte' => $texte,
+]);
